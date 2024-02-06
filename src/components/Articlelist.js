6402294ad9c09cdefdd491af36f3cdeffb635930
Enoch Kibet <enochblake9@gmail.com>
2024-02-06 14:27:30 +0300

    assignment done

--- src/components/Articlelist.js
+++ src/components/Articlelist.js
@@ -1,11 +1,10 @@
-// ArticleList.js
 import React from "react";
 import Article from "./Article";
 
 function ArticleList({ posts }) {
   return (
     <main>
-      {/* Render an array of Article components */}
+      {}
       {posts.map((post) => (
         <Article
           key={post.id}
